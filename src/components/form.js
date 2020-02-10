b8163bfc4b99cb3174b17c9647929830a2a2365b
Trevor Pennington <trevorpennington@trevors-mbp.lan>
2020-02-10 15:21:00 -0500

    form test 4

--- src/components/form.js
+++ src/components/form.js
@@ -27,12 +27,13 @@ export default () => {
               </div>
           </form> */}
 
-          <form name="contact" method="POST" data-netlify="true">
+          <form name="contact" method="POST" netlify="true">
+          <input type="hidden" name="form-name" value="contact" />
   <p>
     <label>Your Name: <input type="text" name="name" /></label>   
   </p>
   <p>
-    <label>Your Email: <input type="email" name="email" /></label>
+    <label>Your Email: <input type="email" name="address" /></label>
   </p>
   <p>
     <label>Your Role: <select name="role[]" multiple>

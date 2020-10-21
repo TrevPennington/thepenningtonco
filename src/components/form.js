97dbeb2c9c8f92b4de6c1c3dfc4498b4c0a0939c
Trevor Pennington <trevorpennington@trevors-mbp.lan>
2020-10-21 08:30:21 -0400

    safari fix, form confirmation

--- src/components/form.js
+++ src/components/form.js
@@ -4,6 +4,10 @@ import { useStaticQuery, graphql } from "gatsby"
 
 export default () => {
 
+  function confirmationAlert() {
+    alert("Thank you for contacting us, we will respond as soon as we can!")
+  }
+
   const helloCopy1 = "Whether we're photographing an engagement, wedding, couple session, etc., Trevor and I always put our heart and soul into capturing photographs you will cherish forever."
   const helloCopy2 = "One of our favorite parts about photographing is creating relationships with our couples, not just being your photographers - but your friends! We strive to make our couples feel appreciated and comfortable. We understand the trust you put in your photographers hands to capture the sweet moments that go by way too fast."
   const helloCopy3 = "Couples who are open for making your photo session an adventure, let's hang out! When photographing we love to capture unique, fun, silly, raw, and romantic moments. Our goal is to reflect the couple's personality and love through simple photographs."
@@ -40,7 +44,7 @@ export default () => {
   
 
           {/* TODO : add a success page, like Squarepatch */}
-          <form name='inquiry' method='POST' className='contactForm' data-netlify="true" data-netlify-honeypot="bot-field" >
+          <form name='inquiry' method='POST' className='contactForm' data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={confirmationAlert}>
                   <input type="hidden" name="bot-field" />
                   <input type="hidden" name="form-name" value="inquiry" />
                   
@@ -58,10 +62,7 @@ export default () => {
                   <input id='f3' className="formField" type='text' name='location' required
                     placeholder="location/venue*"/>
 
-                  {/* <input id='f4' type='date' 
-                    placeholder="date"/> */}
-
-                  <input id='f4' className="formField" type='text' name='date' required placeholder='date' />
+                  <input id='f4' className="formField" type='text' name='date' required placeholder='date*' />
 
                   <textarea id='f5' className="formField" id='moreAbout' name='more about' required
                     placeholder="tell us more about you!*"></textarea>

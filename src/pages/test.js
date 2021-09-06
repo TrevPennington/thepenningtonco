db3d037e6966ea6dd0a34ee9da892b9bdbb24814
TrevPennington <43477475+TrevPennington@users.noreply.github.com>
2021-09-06 12:12:14 -0400

    Update test.js

--- src/pages/test.js
+++ src/pages/test.js
@@ -4,11 +4,7 @@ import { Helmet } from 'react-helmet'
 export default () => {
     return (
         <>
-          <Helmet>
-          <div class='nar-root' data-post-id='fa3e866c-9564-44fd-af47-795b4c755b62' style='p {text-align:center;opacity: 0.0;animation: nara 0s ease-in 2s forwards;}@keyframes nara {to {opacity: 1.0;}}' >
-  <script type="text/javascript" src="https://service.getnarrativeapp.com/core/embed/r/fa3e866c-9564-44fd-af47-795b4c755b62.js"></script>
-</div>
-          </Helmet>
+          
         </>
     )
 }
